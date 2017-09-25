6e23aad2536990d6d8c54b786845a645d179516a
JAMES-HP <u1257434@unimail.hud.ac.uk>
2017-09-25 23:06:57 +0100

    Init commit

---
+++ src/main/java/com/jm/PhilipshuewebappApplication.java
@@ -0,0 +1,12 @@
+package com.jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilipshuewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilipshuewebappApplication.class, args);
+	}
+}
